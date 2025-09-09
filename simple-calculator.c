d09e8869ee8465a2ea55a62a8486a30ddfaf2da0
AishaEniola <aisha.muftau@siu.edu>
2025-09-09 18:51:47 -0500

    Implemented error handling for invalid inputs(some validation doesnt do what it is supposed to do) and added comments

--- simple-calculator.c
+++ simple-calculator.c
@@ -1,3 +1,7 @@
+/*
+Aisha Muftau
+ECE 321 Lab 2
+*/
 #include <stdio.h> 
 //functions prototypes declaration
 double addition(double a, double b);
@@ -10,17 +14,30 @@ int main(){
 
     char choice;        //initializing variable for choice of operation of char type 
     double num1, num2, result; // initializing variables of 2 numbers and result
-
+    
     printf("basic calculator main functions\n");         //main function print
-    printf("enter an operator (+, -, *, /): ");    //list of operations
-    scanf(" %c", &choice);
-    if (choice != '+' && choice != '-' && choice != '*' && choice != '/'){ //check if the choice is valid
+
+//prompting users to enter their desired arithmetic operator   
+    printf("enter an operator (+, -, *, /): ");    
+    if (scanf(" %c", &choice) != 1) {   // Check if a valid character was entered
+        printf("Invalid input! Please enter a valid operator.\n");
+        return 1; 
+    }
+//check if the choice is valid
+    if (choice != '+' && choice != '-' && choice != '*' && choice != '/'){ 
         printf("Invalid operator!\n");
         return 1; // Exit the program with an error code
     }
-    printf("Enter two numbers: ");          //prompt user to enter two numbers  
-    scanf("%lf %lf", &num1, &num2);
 
+//prompt user to enter two numbers and validate the numeric input
+    printf("Enter two numbers: ");          
+    if (scanf("%lf %lf", &num1, &num2) != 2) {                  //check
+        printf("Invalid input! Please enter numeric values.\n");
+        return 1; 
+    }
+    
+
+//Perform calculation
     switch(choice) {
         case '+': printf("Result = %.1lf\n", addition(num1, num2)); break;   //addition operation and addition function call
         case '-': printf("Result = %.1lf\n", subtract(num1, num2)); break;   //subtraction operation and subtraction function call
@@ -30,7 +47,8 @@ int main(){
     }
     return 0;
 }
-//Operations Functions
+
+//Calculator Functions
 
 //addition function
 double addition(double x, double y){
@@ -46,10 +64,10 @@ double multiply(double x, double y){
 }
 //division function
 double division(double x, double y){
-    if(y !=0){          //check if y is not zero
+    if(y !=0){          //check if denominator is not zero
         return x/y;
     } else {
         printf("Error: division by zero can not occur!\n");
-        return 0; // Return 0 
+        return 0; // Return 0 if divided by 0
     }
 }
