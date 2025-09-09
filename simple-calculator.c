ca155421f3495df10ebc96fad835af78558b0a5b
AishaEniola <aisha.muftau@siu.edu>
2025-09-09 14:16:49 -0500

    implemented addition feature

--- simple-calculator.c
+++ simple-calculator.c
@@ -1,6 +1,14 @@
 #include <stdio.h> 
+//function prototypes
+double add(double a, double b);
 
+//main function
 int main(){
-    printf("basic calculator main function\n");
+    printf("basic calculator main functions\n");
+    printf("Addition of 5.5 and 4.5 is: %.1f\n", add(10.5, 4.5));
     return 0;
+}
+//addition fumction
+double add(double x, double y){
+    return x + y;
 }
