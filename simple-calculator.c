f0b7bbd6f00af81ef1d0b71acb4b6585ca0220d3
AishaEniola <aisha.muftau@siu.edu>
2025-09-09 15:46:10 -0500

    Added user interface for calculator operations

--- simple-calculator.c
+++ simple-calculator.c
@@ -7,23 +7,28 @@ double division(double a, double b);
 
 //main function
 int main(){
+
+    char choice;        //initializing variable for choice of operation of char type 
+    double num1, num2, result; // initializing variables of 2 numbers and result
+
     printf("basic calculator main functions\n");         //main function print
+    printf("enter an operator (+, -, *, /): ");    //list of operations
+    scanf(" %c", &choice);
+    printf("Enter two numbers: ");          //prompt user to enter two numbers  
+    scanf("%lf %lf", &num1, &num2);
 
-    double add= addition(10.5,4.5);                     //addition function call
-    printf("Addition of 4.5 and 10.5 is: %.1f\n", add);   // addition print function
-
-    double sub= subtract(10.5,4.5);                  //subtraction function call
-    printf("subtraction of 4.5 from 10.5 is: %.1f\n", sub); //subtraction print function
-
-    double mul= multiply(10.5,4.5);                  //multiplication function call
-    printf("multiplication of 4.5 and 10.5 is: %.1f\n", mul); //multiplication print function
-
-    double div= division(10.5,4.5);                  //division function call
-    printf("division of 10.5 by 4.5 is: %.3f\n", div); //division print function
-
+    switch(choice) {
+        case '+': printf("Result = %.1lf\n", addition(num1, num2)); break;   //addition operation and addition function call
+        case '-': printf("Result = %.1lf\n", subtract(num1, num2)); break;   //subtraction operation and subtraction function call
+        case '*': printf("Result = %.1lf\n", multiply(num1, num2)); break;   //multiplication operation and multiplication function call
+        case '/': printf("Result = %.2lf\n", division(num1, num2)); break;   //division operation and division function call
+        default: printf("Invalid choice.\n");   break; //default case if no valid operation is chosen
+    }
     return 0;
 }
-//addition fumction
+//Operations Functions
+
+//addition function
 double addition(double x, double y){
     return x + y;
 }
@@ -40,7 +45,7 @@ double division(double x, double y){
     if(y !=0){          //check if y is not zero
         return x/y;
     } else {
-        printf("Error: Division by zero!\n");
+        printf("Error: division by zero can not occur!\n");
         return 0; // Return 0 
     }
 }
